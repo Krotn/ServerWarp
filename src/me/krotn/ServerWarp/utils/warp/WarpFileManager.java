563e27e53dc26cb5ed66bdc6e483d09e1276bfa2
Karl Otness <karl@otness.com>
2011-09-10 13:53:44 -0500

    Add WarpFileManager to the project.
    This class will be used to manage
    the warps.txt format for warp storage.

---
+++ src/me/krotn/ServerWarp/utils/warp/WarpFileManager.java
@@ -0,0 +1,11 @@
+package me.krotn.ServerWarp.utils.warp;
+
+import java.io.File;
+
+public class WarpFileManager {
+    File warpFile;
+    
+    public WarpFileManager(File warpFile){
+        this.warpFile = warpFile;
+    }
+}

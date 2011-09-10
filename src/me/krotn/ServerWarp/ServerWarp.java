f92d8c2f5189d94248fe3458bf327af380525886
Karl Otness <karl@otness.com>
2011-09-10 12:37:28 -0500

    Add plugin's main class.

---
+++ src/me/krotn/ServerWarp/ServerWarp.java
@@ -0,0 +1,14 @@
+package me.krotn.ServerWarp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerWarp extends JavaPlugin{
+    
+    public void onEnable(){
+        
+    }
+    
+    public void onDisable(){
+        
+    }
+}

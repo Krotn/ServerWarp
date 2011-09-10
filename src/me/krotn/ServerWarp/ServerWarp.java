2064791d1005395b786c63dc2cf08032544afb12
Karl Otness <karl@otness.com>
2011-09-10 13:41:07 -0500

    ServerWarp now stores a FileManager.

--- src/me/krotn/ServerWarp/ServerWarp.java
+++ src/me/krotn/ServerWarp/ServerWarp.java
@@ -1,11 +1,13 @@
 package me.krotn.ServerWarp;
 
+import me.krotn.ServerWarp.utils.FileManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerWarp extends JavaPlugin{
+    private FileManager fileMan;
     
     public void onEnable(){
-        
+        fileMan = new FileManager(this);
     }
     
     public void onDisable(){

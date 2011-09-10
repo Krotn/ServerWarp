725108cf8cf92d7e9304506c3d9bdebd2c4e219a
Karl Otness <karl@otness.com>
2011-09-10 18:41:40 -0500

    ServerWarp now uses AllWarpManager.

--- src/me/krotn/ServerWarp/ServerWarp.java
+++ src/me/krotn/ServerWarp/ServerWarp.java
@@ -1,16 +1,36 @@
 package me.krotn.ServerWarp;
 
 import me.krotn.ServerWarp.utils.FileManager;
+import me.krotn.ServerWarp.utils.LogManager;
+import me.krotn.ServerWarp.utils.warp.AllWarpManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ServerWarp extends JavaPlugin{
     private FileManager fileMan;
+    private LogManager logMan;
+    private AllWarpManager warpMan;
     
     public void onEnable(){
         fileMan = new FileManager(this);
+        logMan = new LogManager(this.getServer().getLogger(),this);
+        warpMan = new AllWarpManager(fileMan,this.getServer(),logMan);
+        logMan.info("ServerWarp enabled!");
     }
     
     public void onDisable(){
-        
+        try{
+            warpMan.savePublicWarps();
+            logMan.info("Saved warps!");
+        }catch(Exception e){
+            logMan.severe("Error saving warps! Warps may not have been saved!");
+            e.printStackTrace();
+        }
+        try{
+            warpMan.savePlayerWaypoints();
+        }catch(Exception e){
+            logMan.severe("Error saving player waypoints! Player waypoints may not have been saved!");
+            e.printStackTrace();
+        }
+        logMan.info("ServerWarp disabled!");
     }
 }

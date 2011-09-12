8dc97d197970eb5217fe6459f61b0c48450adcde
Karl Otness <karl@otness.com>
2011-09-11 19:03:36 -0500

    Add TeleportHandler to the project.
    This class handles the acutal sotring of tp history.

---
+++ src/me/krotn/ServerWarp/utils/TeleportHandler.java
@@ -0,0 +1,69 @@
+package me.krotn.ServerWarp.utils;
+
+import java.util.Hashtable;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import me.krotn.ServerWarp.utils.history.AllPlayerTeleportHistoryManager;
+
+public class TeleportHandler {
+    private Hashtable<Player,Location> internalTps = new Hashtable<Player,Location>();
+    private AllPlayerTeleportHistoryManager histMan;
+    private PermissionManager permMan;
+    
+    public TeleportHandler(AllPlayerTeleportHistoryManager histMan,PermissionManager permMan){
+        this.histMan = histMan;
+        this.permMan = permMan;
+    }
+    
+    public void doIgnoredTeleport(Player player,Location newLocation){
+        Location oldLocation = player.getLocation();
+        if(permMan.hasPermission(player, "motion.depart")){
+            doForcedIgnoredTeleport(player,newLocation);
+            if(!permMan.hasPermission(player, "motion.arrive")){
+                doForcedIgnoredTeleport(player,oldLocation);
+            }
+        }
+    }
+    
+    protected void doForcedIgnoredTeleport(Player player,Location newLocation){
+        internalTps.put(player, newLocation);
+        player.teleport(newLocation);
+    }
+    
+    protected void ensurePlayerHistManager(Player player){
+        if(!histMan.playerHasHistoryManager(player)){
+            histMan.addHistoryManager(player);
+        }
+    }
+    
+    public void reportTeleport(PlayerTeleportEvent tpEvt){
+        if(internalTps.containsKey(tpEvt.getPlayer())){
+            if(internalTps.get(tpEvt.getPlayer()).equals(tpEvt.getTo())){
+                internalTps.remove(tpEvt.getPlayer());
+                System.out.println("Ignored reported TP!");
+                return;
+            }
+        }
+        ensurePlayerHistManager(tpEvt.getPlayer());
+        histMan.getPlayerHistoryManager(tpEvt.getPlayer()).addToHistory(tpEvt.getTo());
+        System.out.println("Stored reported TP!");
+    }
+    
+    public void doTeleport(Player player,Location newLocation){
+        Location oldLocation = player.getLocation();
+        if(permMan.hasPermission(player, "motion.depart")){
+            internalTps.put(player, newLocation);
+            player.teleport(newLocation);
+            if(!permMan.hasPermission(player, "motion.arrive")){
+                this.doForcedIgnoredTeleport(player,oldLocation);
+            }
+            else{
+                this.ensurePlayerHistManager(player);
+                histMan.getPlayerHistoryManager(player).addToHistory(newLocation);
+            }
+        }
+    }
+}
